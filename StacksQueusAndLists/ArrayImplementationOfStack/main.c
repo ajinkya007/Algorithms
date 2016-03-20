13ab265aa768668a9d06ba6b8596b2b0e1b7f927
ajinkya007 <ajinkya007.in@gmail.com>
2016-03-20 12:41:51 +0530

    Array Implementation of Stack

---
+++ StacksQueusAndLists/ArrayImplementationOfStack/main.c
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.c
+ * Author: Ajinkya Wavare
+ *
+ * Created on September 5, 2014, 5:09 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define size 10
+
+/*
+ * 
+ */
+struct stack {
+    int top;
+    int arr[size];
+};
+int isFull(struct stack *p){
+    return (p->top ==size -1);
+}
+
+int isEmpty(struct stack *p){
+    return (p->top== -1);
+}
+void push(struct stack *p, int val){
+    if(p->top== size-1)
+        printf("Stack Overflow");
+    else 
+        p ->arr[++p->top]= val;
+}
+
+int pop (struct stack *p){
+    if (p->top == -1){
+        printf("Stack Underflow");
+        return 0;
+    }
+    else return p->arr[p->top--];
+}
+int main(int argc, char** argv) {
+    struct stack s;
+    s.top=-1;
+    push(&s,7);
+    push(&s,8);
+    printf("%d \t",pop(&s));
+    return (EXIT_SUCCESS);
+}
+
