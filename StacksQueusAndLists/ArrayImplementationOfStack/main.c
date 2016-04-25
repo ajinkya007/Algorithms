0adaadc1ad8efbe748945573b3c0c17c535ce509
Ajinkya Wavare <ajinkya007@users.noreply.github.com>
2016-04-25 23:46:12 +0530

    Update main.c for Array implementation of stack.

--- StacksQueusAndLists/ArrayImplementationOfStack/main.c
+++ StacksQueusAndLists/ArrayImplementationOfStack/main.c
@@ -12,24 +12,27 @@
 /*
  * 
  */
+ //Define the structure
 struct stack {
     int top;
     int arr[size];
 };
+//Check if the stack is full
 int isFull(struct stack *p){
     return (p->top ==size -1);
 }
-
+//Check if the stack is empty
 int isEmpty(struct stack *p){
     return (p->top== -1);
 }
+//Push on top of the stack
 void push(struct stack *p, int val){
     if(p->top== size-1)
         printf("Stack Overflow");
     else 
         p ->arr[++p->top]= val;
 }
-
+//Remove from top of the stack
 int pop (struct stack *p){
     if (p->top == -1){
         printf("Stack Underflow");
